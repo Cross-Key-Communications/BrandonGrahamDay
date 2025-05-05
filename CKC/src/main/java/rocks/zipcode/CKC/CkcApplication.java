0f58cb9514d2b4624b343eb40d8753369b9eb8e4
ConnorC04 <connorjcasey@iCloud.com>
2025-05-05 10:09:48 -0400

    Make CKC Great Again

---
+++ CKC/src/main/java/rocks/zipcode/CKC/CkcApplication.java
@@ -0,0 +1,12 @@
+package rocks.zipcode.CKC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CkcApplication {
+
+	public static void main(String[] args) {}
+
+}
---
+++ CKC/src/test/java/rocks/zipcode/CKC/CkcApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.CKC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
