23368d8c6ae83cec94e740d7bceac0242b097088
ConnorC04 <connorjcasey@iCloud.com>
2025-05-05 10:53:26 -0400

    even when the door says push, i pull first

--- CKC/src/main/java/rocks/zipcode/CKC/Articles/Articles.java
+++ CKC/src/main/java/rocks/zipcode/CKC/Articles/Articles.java
@@ -1,4 +1,67 @@
 package rocks.zipcode.CKC.Articles;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="articles")
 public class Articles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="Articles_Id")
+    Long id;
+    @Column(name="Articles_Title")
+    String title;
+    @Column(name="Articles_Author")
+    String author;
+    @Column(name="Articles_Thumbnail")
+    String thumbnail;
+    @Column(name="Articles_ArticlesBody")
+    String articleBody;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getArticleBody() {
+        return articleBody;
+    }
+
+    public void setArticleBody(String articleBody) {
+        this.articleBody = articleBody;
+    }
+
+    public Articles(Long id, String title, String author, String thumbnail, String articleBody) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.thumbnail = thumbnail;
+        this.articleBody = articleBody;
+    }
 }
