a5f03dacbc0190a572afb254a4144b0a28269308
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-05-05 10:42:46 -0400

    main working

--- CKC/src/main/java/rocks/zipcode/CKC/CkcApplication.java
+++ CKC/src/main/java/rocks/zipcode/CKC/CkcApplication.java
@@ -2,11 +2,16 @@ package rocks.zipcode.CKC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class CkcApplication {
 
-	public static void main(String[] args) {}
+	public static void main(String[] args) {
+		SpringApplication.run(CkcApplication.class, args);
+
+	}
 
 }
